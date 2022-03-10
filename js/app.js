fa400eb73d4c4b6c2ba41aef01beb7e13b959185
Arafat37 <arafatsarkar22@gmail.com>
2022-03-11 01:53:48 +0600

    fixed q home tab

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById('questionSection').style.display = "block";
         
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
